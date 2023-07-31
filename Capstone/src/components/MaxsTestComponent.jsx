c96e7d03891a28f3fabe3afc54e457f7bc33a18b
MaxwellConway <maxconway23@gmail.com>
2023-07-31 16:49:11 -0400

    added API data to the carousel component
    added API data to the carousel component

--- Capstone/src/components/MaxsTestComponent.jsx
+++ Capstone/src/components/MaxsTestComponent.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const MaxsTestComponent = () => {
   // Step 2: Create a state variable to hold the fetched data
-  const [data, setData] = useState([]);
+  const [games, setGames] = useState([]);
 
   // Step 3: Use useEffect to trigger the API request
   useEffect(() => {
@@ -10,12 +10,12 @@ const MaxsTestComponent = () => {
       try {
         // Make the API request
         const response = await fetch(
-          'https://api.rawg.io/api/games?key=708a4757c9d748448c87455a3ecd365c&search="dark%20souls"'
+          "https://api.rawg.io/api/games?key=708a4757c9d748448c87455a3ecd365c&metacritic=90,100&ordering=-released&dates=2005-01-01,2023-07-01&tags=multiplayer&page_size=10"
         );
         const jsonData = await response.json();
 
         // Step 4: Update the state with the fetched data
-        setData(jsonData.results);
+        setGames(jsonData.results);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -27,12 +27,23 @@ const MaxsTestComponent = () => {
   // Step 5: Render the data
   return (
     <div>
-      <h1>Dark Souls Games:</h1>
-      <ul>
-        {data.map((game) => (
-          <li key={game.id}>{game.name}</li>
+      <h1>Top 10 Games with Metacritic Score of 90 or above:</h1>
+      <div className="grid grid-cols-2 gap-4">
+        {/* Assuming you want two images per row, adjust grid-cols-* accordingly */}
+        {games.map((game) => (
+          <div key={game.id} className="flex flex-col items-center">
+            <h2>{game.name}</h2>
+            <div className="w-40 h-52">
+              {/* Define a container size, e.g., w-250 and h-160 */}
+              <img
+                src={game.background_image}
+                alt={game.name}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
