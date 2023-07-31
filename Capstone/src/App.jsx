d4fef3140461dd8b4a7c335db013ee3251048f42
kimwells00 <wellskimberly22@gmail.com>
2023-07-31 18:10:53 -0400

    invoked 2 components
    invoked AvatarModal component and createUserForm component

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import "./App.css";
+import AvatarModal from "./components/AvatarModal";
 import ChatMessage from "./components/ChatMessage";
 import CreateUserForm from "./components/CreateUserForm";
 import Friends from "./components/Friends";
@@ -9,23 +10,24 @@ import MaxsTestComponent from "./components/MaxsTestComponent";
 import Navbar from "./components/Navbar";
 import Users from "./components/Users";
 import LinkedGames from "./components/linkedGames";
-// import Carousel from "./components/Carousel"
+import Carousel from "./components/Carousel";
 // import { useState, useEffect } from "react";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<Users />
-			<Friends />
-			<Games />
-			<LinkedGames />
-			<ChatMessage />
-			<MaxsTestComponent />
-			<CreateUserForm />
-			{/* <Carousel /> */}
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Users />
+      <Friends />
+      <Games />
+      <LinkedGames />
+      <ChatMessage />
+      <MaxsTestComponent />
+      <Carousel />
+      <AvatarModal />
+      <CreateUserForm />
+    </>
+  );
 }
 
 export default App;
