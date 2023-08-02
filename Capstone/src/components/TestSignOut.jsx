d2dde5aa4b0b34d537024c7cddd96f6b6d3c6e0d
MaxwellConway <maxconway23@gmail.com>
2023-08-02 14:50:53 -0400

    added supabase auth component

---
+++ Capstone/src/components/TestSignOut.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import supabase from "../../../supabase";
+
+const TestSignOut = () => {
+  const handleSignOut = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        console.error("Error signing out:", error.message);
+      } else {
+        console.log("User signed out successfully");
+        // Optionally, you can redirect the user to a different page after successful sign-out.
+        // Example: window.location.href = '/login';
+      }
+    } catch (error) {
+      console.error("Error signing out:", error.message);
+    }
+  };
+
+  return <button onClick={handleSignOut}>Sign Out</button>;
+};
+
+export default TestSignOut;
