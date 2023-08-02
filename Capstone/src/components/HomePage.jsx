e61d133790db612cd4147698d57a705ce0304c42
kimwells00 <wellskimberly22@gmail.com>
2023-08-02 14:58:57 -0400

    updated components

--- Capstone/src/components/HomePage.jsx
+++ Capstone/src/components/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import CreateUserForm from "./CreateUserForm";
 import Carousel from "./Carousel";
+import HomeCarouselOne from "./HomeCarouselOne";
 
 const HomePage = () => {
   return (
-    <div>
-      <CreateUserForm />
-      <Carousel />
+    <div className="HomePage">
+      {/* <CreateUserForm />  
+      <Carousel /> */}
+      <h2 className="NewReleases">New and trending</h2>
+      <HomeCarouselOne />
+      <h2 className="NewReleases">Recently released</h2>
+      <HomeCarouselOne />
     </div>
   );
 };
