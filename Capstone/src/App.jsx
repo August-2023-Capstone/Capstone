0d318001f8f163868f0a5a29d4bccdab0d53530a
MaxwellConway <maxconway23@gmail.com>
2023-08-04 10:30:49 -0400

    reverted app.jsx

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -8,20 +8,12 @@ import HomePage from "./components/HomePage";
 import UsersPage from "./components/UsersPage";
 import FriendSearchPage from "./components/FriendSearchPage";
 import ChatMessagePage from "./components/ChatMessagePage";
-import TestLogIn2 from "./components/TestLogIn2";
-import TestSignOut from "./components/TestSignOut";
-import CreateUserForm from "./components/CreateUserForm";
-import TestProfile from "./components/TestProfile";
 // import { useState, useEffect } from "react";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <TestLogIn2 />
-      <TestSignOut />
-      <TestProfile />
-      <CreateUserForm />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/users" element={<UsersPage />} />
