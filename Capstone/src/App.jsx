f28373f4f25024b0eb1d2d7cf6f14f55170dd14d
kimwells00 <wellskimberly22@gmail.com>
2023-08-07 15:50:47 -0400

    changed to render create user form component

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -10,6 +10,7 @@ import FriendSearchPage from "./components/FriendSearchPage";
 import ChatMessagePage from "./components/ChatMessagePage";
 import ProfilePage from "./components/ProfilePage";
 import TestLogIn2 from "./components/TestLogIn2";
+import CreateUserForm from "./components/CreateUserForm";
 
 // import { useState, useEffect } from "react";
 
@@ -18,12 +19,13 @@ function App() {
     <Router>
       <Navbar />
       {/* <TestLogIn2 /> */}
+      <CreateUserForm />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/settings" element={<FriendSearchPage />} />
-        <Route path="/chat" element={<ChatMessagePage />} />
+        <Route path="/chat" element={<ChatMessagePage />} /> */}
       </Routes>
     </Router>
   );
