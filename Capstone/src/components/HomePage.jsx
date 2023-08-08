91970e4b6f75530fc94b49f92f6ffa27b8388006
dbaron5 <131029961+dbaron5@users.noreply.github.com>
2023-08-08 17:10:16 -0400

    Logout button fixed

--- Capstone/src/components/HomePage.jsx
+++ Capstone/src/components/HomePage.jsx
@@ -6,17 +6,17 @@ import Login from "./Login";
 
 const HomePage = () => {
   const [showLoginModal, setShowLoginModal] = useState(false);
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      const userIsLoggedIn = false;
-      if (!userIsLoggedIn) {
+      if (!session) {
         setShowLoginModal(true);
       }
     }, 5000);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [session]);
 
   return (
     <div className="HomePage">
@@ -31,7 +31,7 @@ const HomePage = () => {
           className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex items-center justify-center z-10"
         >
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <Login />
+            <Login session={session} />
           </div>
         </div>
       )}
