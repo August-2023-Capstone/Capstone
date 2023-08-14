ff823b42f491cacc66a849318cab18e14b966d44
kimwells00 <wellskimberly22@gmail.com>
2023-08-14 15:45:40 -0400

    styling updates

--- Capstone/src/components/ProfileData.jsx
+++ Capstone/src/components/ProfileData.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import supabase from "../../../supabase";
 import CreateUserForm from "./CreateUserForm";
-
+import Avatar1 from "../assets/Avatars/Avatar1.png";
+import edit from "../assets/icons/edit.png";
 const ProfileData = () => {
   const [profileData, setProfileData] = useState([]);
   const [showCreateUserForm, setShowCreateUserForm] = useState(false);
@@ -36,10 +37,14 @@ const ProfileData = () => {
       {/* <h1>Profiles List</h1> */}
       {profileData.map((profile) => (
         <div key={profile.id} className="ProfileData">
-          <p className="ProfileGamertag">Gamertag: {profile.gamertag}</p>
-          <p className="ProfileTimezone">Timezone: {profile.timezone}</p>
+          <img src={Avatar1} alt="" className="profileAvatar" />
+          <div className="profileDataMiddleContainer">
+            <p className="ProfileGamertag">Gamertag: {profile.gamertag}</p>
+            <p className="ProfileTimezone">Timezone: {profile.timezone}</p>
 
-          <p className="ProfilePlatform">Platform: {profile.platform}</p>
+            <p className="ProfilePlatform">Platform: {profile.platform}</p>
+          </div>
+          <img src={edit} alt="" className="editProfileIcon" />
 
           {/* Add other profile properties as needed */}
         </div>
